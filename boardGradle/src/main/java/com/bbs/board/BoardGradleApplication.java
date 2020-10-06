8066820accd3ae9f3bb2eec60df2de3e099bf15e
B202 <B202@B202-15>
2020-10-06 11:30:41 +0900

    최초업로드

---
+++ boardGradle/src/main/java/com/bbs/board/BoardGradleApplication.java
@@ -0,0 +1,13 @@
+package com.bbs.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardGradleApplication.class, args);
+	}
+
+}
---
+++ boardGradle/src/test/java/com/bbs/board/BoardGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbs.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
